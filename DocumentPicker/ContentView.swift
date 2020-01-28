006559b118b11f805bd7b1d4fa29737a33154eca
Dhanush <dhanush@dhanush.corp.capgemini.com>
2020-01-28 17:13:24 +0530

    Final Commit

--- DocumentPicker/ContentView.swift
+++ DocumentPicker/ContentView.swift
@@ -7,10 +7,24 @@
 //
 
 import SwiftUI
+import MobileCoreServices
+import Firebase
 
 struct ContentView: View {
+    @State var show = false
+    @State var alert = false
     var body: some View {
-        Text("Hello World")
+        Button(action : {
+            self.show.toggle()
+        }){
+            Text("Document Picker")
+        }
+        .sheet(isPresented: $show){
+            DocumentPicker(alert: self.$alert)
+        }
+        .alert(isPresented: $alert){
+            Alert(title: Text("Message"), message: Text("Uploaded Successfully"), dismissButton: .default(Text("Ok")))
+        }
     }
 }
 
@@ -19,3 +33,44 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct DocumentPicker : UIViewControllerRepresentable {
+    func makeCoordinator() -> DocumentPicker.Coordinator {
+        return DocumentPicker.Coordinator(parent1: self)
+    }
+    
+    @Binding var alert : Bool
+    func makeUIViewController(context: UIViewControllerRepresentableContext<DocumentPicker>) -> UIDocumentPickerViewController {
+        // we can change that String(KUTTypeItem / PDF / MP3) based on which document You want to pick
+        let picker = UIDocumentPickerViewController(documentTypes: [String(kUTTypeItem)], in: .open)
+        picker.allowsMultipleSelection = false
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: UIViewControllerRepresentableContext<DocumentPicker>) {
+        
+    }
+    
+    class Coordinator: NSObject,UIDocumentPickerDelegate {
+        var parent : DocumentPicker
+        init (parent1 : DocumentPicker){
+            parent = parent1
+        }
+        
+        func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+            let bucket = Storage.storage().reference()
+            bucket.child((urls.first?.deletingPathExtension().lastPathComponent)!).putFile(from: urls.first!, metadata: nil) { (_, err) in
+                if err != nil{
+                    print((err?.localizedDescription)!)
+                    return
+                }
+                print("Success")
+                self.parent.alert.toggle()
+                
+            }
+        }
+    }
+}
+
+// after that check in firebase for the document u are uploaded in the App in Storage section of firebase
---
+++ Pods/Target Support Files/Pods-DocumentPicker-DocumentPickerUITests/Pods-DocumentPicker-DocumentPickerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DocumentPicker_DocumentPickerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DocumentPicker_DocumentPickerUITests
+@end
